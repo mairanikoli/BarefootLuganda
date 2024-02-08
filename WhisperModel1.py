f11b4a40ff252b2317c39736ab22b4b9073cc434
mairanikoli <115565386+mairanikoli@users.noreply.github.com>
2024-02-08 16:13:33 +0000

    Update WhisperModel1.py
    removed duplication error

--- WhisperModel1.py
+++ WhisperModel1.py
@@ -24,30 +24,6 @@ common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
 
 #common_voice = common_voice.map(resample_audio)
 
-def prepare_dataset(batch):
-    # load and resample audio data from 48 to 16kHz
-    audio = batch["audio"]
-
-    # compute log-Mel input features from input audio array 
-    from transformers import WhisperFeatureExtractor
-    feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
-
-    from transformers import WhisperTokenizer
-    #I have chosen Swahili in the same family as Luganda
-    tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-small", task="transcribe")
-
-    batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
-
-    # encode target text to label ids 
-    batch["labels"] = tokenizer(batch["sentence"]).input_ids
-    return batch
-    
-common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["train"], num_proc=4)
-
-#downsample to match whisper sampling rate
-from datasets import Audio
-common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
-
 def prepare_dataset_general(batch):
     # load and resample audio data from 48 to 16kHz
     audio = batch["audio"]
