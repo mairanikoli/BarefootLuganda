57aa586c3bc455cad709f5fb4d2d4817ffaec7a6
mairanikoli <mn720@ic.ac.uk>
2024-02-15 15:30:45 +0000

    version 16 again

--- WhisperModel2.py
+++ WhisperModel2.py
@@ -8,9 +8,9 @@ from datasets import load_dataset, DatasetDict
 
 common_voice = DatasetDict()
 
-common_voice["train"] = load_dataset("mozilla-foundation/common_voice_15_0", "lg", split="train", trust_remote_code=True)
-common_voice["validation"] = load_dataset("mozilla-foundation/common_voice_15_0", "lg", split="validation", trust_remote_code=True)
-common_voice["test"] = load_dataset("mozilla-foundation/common_voice_15_0", "lg", split="test", trust_remote_code=True)
+common_voice["train"] = load_dataset("mozilla-foundation/common_voice_16_0", "lg", split="train", trust_remote_code=True)
+common_voice["validation"] = load_dataset("mozilla-foundation/common_voice_16_0", "lg", split="validation", trust_remote_code=True)
+common_voice["test"] = load_dataset("mozilla-foundation/common_voice_16_0", "lg", split="test", trust_remote_code=True)
 
 common_voice = common_voice.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "segment", "up_votes"])
 
